9b327fafa049eecfaaaf1a2e32fc999d229ba15b
Ly_Man <lym861@hsd.k12.or.us>
2025-05-15 10:27:10 -0700

    cleaning up the movement scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20.0f;
+    public float horizontalInput;
+    private float xRange = 10;
+    public Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    // Moves the player based on arrow key input
+    void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right *horizontalInput * Time.deltaTime * speed);
+    }
+
+    // Prevents the player movingf out off the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        } 
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } 
     }
 }
